b0128afba82102b00780c21a9ece1f73c269f536
Steen Clarke <cmclarkey@gmail.com>
2016-10-12 13:04:15 -0700

    Change arrays to hashes in probabilities hash, so probabilities are easier to get out

--- blog-scraper/blog-scraper.rb
+++ blog-scraper/blog-scraper.rb
@@ -7,6 +7,8 @@ all_words_array = []
 probabilities_hash = {}
 word_probabilities_array = []
 
+all_words_by_all_words_hash = {}
+
 File.open("blog.txt", 'r') do |file|
   file.each_line do |line|
     line_words_array = line.split(" ")
@@ -24,10 +26,17 @@ end
 
 words_hash.each do |word, following_words|
   # file.write("#{word}\t#{following_words}\n")
-  probabilities_hash[word] = []
   following_words.each do |following_word|
     float_holder = 1.to_f/following_words.length.to_f
-    probabilities_hash[word] << [following_word, BigDecimal.new(float_holder, 4)]
+    if probabilities_hash[word]
+      if probabilities_hash[word][:"#{following_word}"]
+        probabilities_hash[word][:"#{following_word}"] += BigDecimal.new(float_holder, 4)
+      else
+        probabilities_hash[word][:"#{following_word}"] = BigDecimal.new(float_holder, 4)
+      end
+    else
+      probabilities_hash[word] = {"#{following_word}": BigDecimal.new(float_holder, 4)}
+    end
   end
   all_words_array << word
 end
@@ -36,8 +45,16 @@ file = File.open("word_breakdown.txt", 'w')
 
 probabilities_hash.each do |word, probabilities|
   file.write("#{word};\t")
-  probabilities.each do |probability|
-    file.write("#{probability[0]}: #{probability[1].truncate(2).to_s('F')}, ")
+  probabilities.each do |following_word, probability|
+    file.write("#{following_word}: #{probability.truncate(4).to_s('F')}, ")
   end
   file.write("\n")
 end
+
+# all_words_array.each do |word|
+#   all_words_by_all_words_hash[word] = all_words_array
+# end
+
+# all_words_by_all_words_hash.each do |word, following_word|
+#   if words_hash[word].include? following_word
+# end
