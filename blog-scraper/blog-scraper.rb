308fc432943d0cf622b89a34dfdf9570aa72be9d
Steen Clarke <cmclarkey@gmail.com>
2016-10-12 15:18:29 -0700

    Work around issue of the randomizer array getting whitespace in it somehow by using that as a time to break the paragraph.

--- blog-scraper/blog-scraper.rb
+++ blog-scraper/blog-scraper.rb
@@ -13,15 +13,17 @@ randomizer_words_array = []
 File.open("blog.txt", 'r') do |file|
   file.each_line do |line|
     line_words_array = line.split(" ")
-    line_words_array.each_with_index do |word, word_index|
-      if words_hash[word] && word_index <= line_words_array.length
-        words_popularity_hash[word] += 1
-        words_hash[word] << line_words_array[word_index+1]
-      elsif words_hash[word]
-        words_popularity_hash[word] += 1
-      else
-        words_hash[word] = [line_words_array[word_index+1]]
-        words_popularity_hash[word] = 1
+    if line_words_array.any?
+      line_words_array.each_with_index do |word, word_index|
+        if words_hash["#{word}"] && word_index < line_words_array.length-1
+          words_popularity_hash["#{word}"] += 1
+            words_hash["#{word}"] << line_words_array[word_index+1]
+        elsif words_hash["#{word}"]
+          words_popularity_hash["#{word}"] += 1
+        elsif word_index < line_words_array.length-1
+          words_hash["#{word}"] = [line_words_array[word_index+1]]
+          words_popularity_hash["#{word}"] = 1
+        end
       end
     end
   end
@@ -31,14 +33,14 @@ words_hash.each do |word, following_words|
   # file.write("#{word}\t#{following_words}\n")
   following_words.each do |following_word|
     float_holder = 1.to_f/following_words.length.to_f
-    if probabilities_hash[word]
-      if probabilities_hash[word]["#{following_word}"]
-        probabilities_hash[word]["#{following_word}"] += 1
+    if probabilities_hash["#{word}"]
+      if probabilities_hash["#{word}"]["#{following_word}"]
+        probabilities_hash["#{word}"]["#{following_word}"] += 1
       else
-        probabilities_hash[word]["#{following_word}"] = 1
+        probabilities_hash["#{word}"]["#{following_word}"] = 1
       end
     else
-      probabilities_hash[word] = {"#{following_word}" => 1}
+      probabilities_hash["#{word}"] = {"#{following_word}" => 1}
     end
   end
     all_words_array << word
@@ -54,25 +56,21 @@ random_word = all_words_array.sample
 
 print "#{random_word} "
 500.times do
-  probabilities_hash[random_word].each do |following_word, probability|
-    probability.times do
-      randomizer_words_array << following_word
+  if probabilities_hash[random_word]
+    probabilities_hash[random_word].each do |following_word, probability|
+      probability.times do
+        randomizer_words_array << following_word
+      end
     end
+    random_word = randomizer_words_array.sample
+    print "#{random_word} "
+    randomizer_words_array = []
+  else
+    puts "\n"
+    random_word = all_words_array.sample
   end
-  random_word = randomizer_words_array.sample
-  print "#{random_word} "
-  randomizer_words_array = []
 end
 
-# Is it necessary to explicitly list all probabilities of 0????
-###############################################################
-# all_words_array.each do |compare_word|
-#   probabilities_hash.each do |first_word, following_word|
-#     unless probabilities_hash[first_word][:"#{compare_word}"]
-#       probabilities_hash[first_word][:"#{compare_word}"] = BigDecimal.new(0)
-#     end
-#   end
-# end
 
 file = File.open("word_breakdown.txt", 'w')
 
