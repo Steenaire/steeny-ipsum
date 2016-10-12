0df9dec05be39d6ef0b6827be15ec27af51b6fa6
Steen Clarke <cmclarkey@gmail.com>
2016-10-11 17:41:18 -0700

    add probabilities for each following word

--- blog-scraper/blog-scraper.rb
+++ blog-scraper/blog-scraper.rb
@@ -1,5 +1,11 @@
+require 'bigdecimal'
+
 words_hash = {}
 line_words_array = []
+all_words_array = []
+
+probabilities_hash = {}
+word_probabilities_array = []
 
 File.open("blog.txt", 'r') do |file|
   file.each_line do |line|
@@ -16,8 +22,22 @@ File.open("blog.txt", 'r') do |file|
   end
 end
 
+words_hash.each do |word, following_words|
+  # file.write("#{word}\t#{following_words}\n")
+  probabilities_hash[word] = []
+  following_words.each do |following_word|
+    float_holder = 1.to_f/following_words.length.to_f
+    probabilities_hash[word] << [following_word, BigDecimal.new(float_holder, 4)]
+  end
+  all_words_array << word
+end
+
 file = File.open("word_breakdown.txt", 'w')
 
-words_hash.each do |word, following_words|
-  file.write("#{word}\t#{following_words}\n")
+probabilities_hash.each do |word, probabilities|
+  file.write("#{word};\t")
+  probabilities.each do |probability|
+    file.write("#{probability[0]}: #{probability[1].truncate(2).to_s('F')}, ")
+  end
+  file.write("\n")
 end
